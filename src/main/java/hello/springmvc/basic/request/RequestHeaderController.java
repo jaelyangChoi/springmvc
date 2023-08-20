9981889fa951e890c257636936059ffc9c5fc798
Jealyang <jaelyangchoi@gmail.com>
2023-08-20 22:11:00 +0900

    애노테이션 기반 컨트롤러는 다양한 파라미터를 받아들일 수 있다! - Http 요청 헤더 조회 예제

--- src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Locale;
 
+/** 애노테이션 기반 컨트롤러는 다양한 파라미터를 받아들일 수 있다!
+* */
 //private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(RequestHeaderController.class);
 @Slf4j //위 코드를 자동으로 생성해서 로그를 선언해준다. 개발자는 편리하게 log 라고 사용하면 된다.
 @RestController
@@ -22,11 +24,11 @@ public class RequestHeaderController {
     public String headers(HttpServletRequest request,
                           HttpServletResponse response,
                           HttpMethod httpMethod,
-                          Locale locale,
-                          //MultiValueMap : 하나의 키에 여러 값을 받을 수 있다. HTTP header, HTTP 쿼리 파라미터와 같이 하나의 키에 여러 값을 받을 때 사용
+                          Locale locale, //언어정보
+                          //MultiValueMap : 하나의 키에 여러 값을 받을 수 있다. HTTP header, HTTP 쿼리 파라미터와 같이 하나의 키에 여러 값을 받을 때 사용. ketA=value1 & keyA=value2
                           @RequestHeader MultiValueMap<String, String> headerMap,
                           @RequestHeader("host") String host,
-                          @CookieValue(value = "cookieName", required = false) String cookie) {
+                          @CookieValue(value = "cookieName", required = false) String cookie) { //value : 쿠키 이름
         log.info("request={}", request);
         log.info("response={}", response);
         log.info("httpMethod={}", httpMethod);
